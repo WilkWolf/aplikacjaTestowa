24060720d8e15168d80fc6c1a833c4cfb847947f
WilkWolf <48549453+WilkWolf@users.noreply.github.com>
2023-10-01 20:19:52 +0200

    Renamed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ControlsEnabledComponent } from './controls-enabled/controls-enabled.component';
+import { StandardControlsComponent } from './standard-controls/standard-controls.component';
 import { ControlsDisabledComponent } from './controls-disabled/controls-disabled.component';
-import { ControlsWithStaticIdComponent } from './controls-with-static-id/controls-with-static-id.component';
+import { DuplicatedControlsComponent } from './duplicated-controls/duplicated-controls.component';
 import { ControlsWithDynamicIDComponent } from './controls-with-dynamic-id/controls-with-dynamic-id.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from './material/material.module';
@@ -14,9 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
-    ControlsEnabledComponent,
+    StandardControlsComponent,
     ControlsDisabledComponent,
-    ControlsWithStaticIdComponent,
+    DuplicatedControlsComponent,
     ControlsWithDynamicIDComponent
   ],
   imports: [
@@ -25,8 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: 'controlsDisabled', component: ControlsDisabledComponent },
-      { path: 'controlsEnabled', component: ControlsEnabledComponent },
-      { path: 'controlsWithStaticID', component: ControlsWithStaticIdComponent },
+      { path: 'standardControls', component: StandardControlsComponent },
+      { path: 'duplicatedControls', component: DuplicatedControlsComponent },
       { path: 'controlsWIthDynamicID', component: ControlsWithDynamicIDComponent },
     ]),
     MaterialModule,
