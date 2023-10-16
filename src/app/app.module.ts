69aaff5f39d7a862654a329efb12fd1aa6692287
WilkWolf <48549453+WilkWolf@users.noreply.github.com>
2023-10-16 23:12:24 +0200

    Changed link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     RouterModule.forRoot([
       { path: 'standardControls', component: StandardControlsComponent },
       { path: 'duplicatedControls', component: DuplicatedControlsComponent },
-      { path: 'controlsWIthDynamicID', component: VariousControlsIdentificationComponent },
+      { path: 'variousFormOfIdentification', component: VariousControlsIdentificationComponent },
       { path: '', redirectTo: 'standardControls', pathMatch: 'full' },
       { path: '**', redirectTo: 'standardControls', pathMatch: 'full' },
     ]),
